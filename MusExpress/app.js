2e2e61193dff36d0cbfbae58774c110db94a6414
Morane.Otilia <morane.gg@gmail.com>
2016-10-14 00:04:55 +0200

    Updated javascript

--- MusExpress/app.js
+++ MusExpress/app.js
@@ -1,7 +1,23 @@
-function test()
-{
-	if(getElementById("nom").equals(""))
-		{
-		
-		}
-}
+ $(document).ready(function(){
+       $('#login').click(function()
+       {   
+          var nom=$('#nom').val();
+          var prenom=$('#prenom').val();
+	  var codep=$('#codep').val();
+          var email=$('#email').val();
+	  var password=$('#password').val();
+          
+          $.ajax({
+               type: "POST",
+               url:"InscriptionServlet",
+               data:{"nom":nom, "prenom": prenom, "codep":codep,"email":email,"password":password},
+               success: function (data) {
+                  if(data=='True'){
+                    alert('success');
+                  }else{
+                      alert('Fail....');
+                  }
+               }
+             });                                
+           });
+         });
