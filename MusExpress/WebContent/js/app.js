ff27fbe04c274e4c685fb50d4f7e41bf0f6d3e91
Morane.Otilia <morane.otilia@gmail.com>
2016-11-05 02:22:59 +0100

    ajouter d'une map avec marker en dur (louvre)

--- MusExpress/WebContent/js/app.js
+++ MusExpress/WebContent/js/app.js
@@ -9,7 +9,8 @@ $( document ).ready(function(){
 
 	var idMusee = GetURLParameter('id_musee');
 	$(".navbar-brand").click(routeur.index);
-
+	
+	
 });
 
 var home = {
@@ -199,5 +200,79 @@ function afficheMusee(musees, eltDomList){
         $(".musee").click(routeur.musee(event.target.id));
 	   });
 }
+/**
+ * Affichage de google map
+ */
+function initMap() {
+	  var museeActuel = {lat: 48.8596, lng: 2.3369};
+	console.log("map");
+	  var map = new google.maps.Map(document.getElementById('map'), {
+		    center: museeActuel,
+		zoom: 14,
+		panControl: false,
+		scrollwheel: false,
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+		  });
 
+	  var marker = new google.maps.Marker({
+		    position: museeActuel,
+		    map: map,
+		    title: 'Le Louvre'
+			  });
+
+	var contentString = '<div class="info-window">' +
+	'<h3>Info Window Content</h3>' +
+	'<div class="info-content">' +
+	'<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>' +
+	'</div>' +
+	'</div>';
+
+	var infowindow = new google.maps.InfoWindow({
+		content: contentString,
+		maxWidth: 400
+	});
+
+	marker.addListener('click', function () {
+		infowindow.open(map, marker);
+	});
+
+	var styles = [{"featureType": "landscape", "stylers": [{"saturation": -100}, {"lightness": 65}, {"visibility": "on"}]}, {"featureType": "poi", "stylers": [{"saturation": -100}, {"lightness": 51}, {"visibility": "simplified"}]}, {"featureType": "road.highway", "stylers": [{"saturation": -100}, {"visibility": "simplified"}]}, {"featureType": "road.arterial", "stylers": [{"saturation": -100}, {"lightness": 30}, {"visibility": "on"}]}, {"featureType": "road.local", "stylers": [{"saturation": -100}, {"lightness": 40}, {"visibility": "on"}]}, {"featureType": "transit", "stylers": [{"saturation": -100}, {"visibility": "simplified"}]}, {"featureType": "administrative.province", "stylers": [{"visibility": "off"}]}, {"featureType": "water", "elementType": "labels", "stylers": [{"visibility": "on"}, {"lightness": -25}, {"saturation": -100}]}, {"featureType": "water", "elementType": "geometry", "stylers": [{"hue": "#ffff00"}, {"lightness": -25}, {"saturation": -97}]}];
+
+	map.set('styles', styles);
+
+
+	google.maps.event.addDomListener(window, 'load', initMap);
+	var aProximite = [{
+		nomMusee: 'test1',
+		localisation : {
+			lat: 48.8592, 
+			lng: 2.3379
+		},
+	},{
+		nomMusee: 'test2',
+		localisation : {
+			lat: 48.8567, 
+			lng: 2.3359
+		}
+	},{
+		nomMusee: 'test3',
+		localisation : {
+			lat: 48.8567, 
+			lng: 2.3349
+		}
+	}];
+
+	for(i=0; i<aProximite.length; i++)
+	{
+		new google.maps.Marker({
+			    position: aProximite[i].localisation,
+			    map: map,
+			    title: aProximite[i].nomMusee,
+			icon: 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png'
+				  });
+	}
+
+
+
+}
 
